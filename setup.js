76444a69b15aec6c0ca04584493e184e4b26e553
nullx <mail@nullx.me>
2020-05-10 13:45:30 +0200

    Not execute permissions of macOS binary on other os

--- setup.js
+++ setup.js
@@ -1,8 +1,12 @@
 const exec = require('child_process').exec;
 const file_d = __dirname + '/mac' + '/window';
+const os = require('os');
 
-
-exec("chmod +x " + file_d, (error, stdout, stderr) => {
-    if (error) return process.exit(2);
+if (os.platform() == 'darwin'){
+    exec("chmod +x " + file_d, (error, stdout, stderr) => {
+        if (error) return process.exit(2);
+        process.exit(0);
+    });
+} else {
     process.exit(0);
-});
+}
